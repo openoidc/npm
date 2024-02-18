0b4c61ee040b286e0175e651388f421fa7ad86b1
Johannes Waigel <johannes@waigel.me>
2024-02-18 00:16:19 +0000

    fix: Remove falsy commited build folder

--- packages/server/build/src/lib/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-var shared_1 = require("@maybe-finance/server/shared");
-var logger = (0, shared_1.createLogger)({
-    level: (_a = process.env.LOG_LEVEL) !== null && _a !== void 0 ? _a : 'info',
-});
-exports.default = logger;
